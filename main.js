a152924ac73b84e3de805d693be344d7b5ff6f98
kaori02 <kaorigaming02@gmail.com>
2020-10-12 09:38:34 +0700

    Berhasil buat persegi 2 warna dengan gl.TRIANGLES

--- main.js
+++ main.js
@@ -8,17 +8,17 @@ function main()
    * A (-0.5, 0.5); B(-0.5, -0.5); C(0.5, -0.5); D(0.5, 0.5)
    */
   var vertices = [
-    -0.5, 0.5,    // A 
-    -0.5, -0.5,   // B
-    0.5, -0.5,    // C
-    0.5, -0.5,    // C
-    0.5, 0.5,     // D
-    -0.5, 0.5,    // A 
+    -0.5, 0.5, 1.0, 0.0, 0.0,    // A 
+    -0.5, -0.5, 1.0, 0.0, 0.0,   // B
+    0.5, -0.5, 1.0, 0.0, 0.0,    // C
+    0.5, -0.5, 0.0, 0.0, 1.0,    // C
+    0.5, 0.5, 0.0, 0.0, 1.0,    // D
+    -0.5, 0.5, 0.0, 0.0, 1.0,    // A 
 
   ];
 
-  var positionBuffer = gl.createBuffer(); //pointer ke gl di alam gpu
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  var vertexBuffer = gl.createBuffer(); //pointer ke gl di alam gpu
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
@@ -40,14 +40,17 @@ function main()
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
   var aPosition = gl.getAttribLocation(shaderProgram, "a_position");
-  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+  var aColor = gl.getAttribLocation(shaderProgram, "a_color");
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 0);
+  gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
   gl.enableVertexAttribArray(aPosition);
+  gl.enableVertexAttribArray(aColor);
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.viewport(100, 0, canvas.height, canvas.height)
+  gl.viewport(100, 0, canvas.height, canvas.height);
 
   var primitive = gl.TRIANGLES;
   var offset = 0;
